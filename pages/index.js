87311e739fb71d62b74e6fc6a742db3ec294f7b1
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-05-10 23:04:36 -0700

    Create link page

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,10 @@
-import React from 'react'
-import { bindActionCreators } from 'redux'
-import { initStore, startUpdates, hackerWatcher } from '../store'
-import withRedux from './utils/withRedux'
-import Map from './components/Map'
-import AreaGraph from './components/AreaGraph'
-import Block from './components/Block'
+import Link from 'next/link'
 
-class Dashboard extends React.Component {
-  static getInitialProps ({ store, isServer }) {
-    return { isServer }
-  }
-
-  shouldComponentUpdate(nextProps) {
-    if (nextProps.peers.length !== this.props.peers.length) {
-      return true
-    }
-
-    if (nextProps.latestBlock !== this.props.latestBlock) {
-      return true
-    }
-
-    return false
-  }
-
-  componentDidMount () {
-    this.interval = this.props.startUpdates()
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
-
-  renderCurrentGasPrice() {
-    return (
-      <div>
-        <h1>Median Gas Price</h1>
-        <h3>{ this.props.currentGasPrice } ETH</h3>
-      </div>
-    )
-  }
-
-  render () {
-    const { peers, latestBlock } = this.props
-    const { hash, number } = latestBlock
-
-    return (
-      <div>
-        <Block title="Latest Block" block={this.props.latestBlock} showDifficulty />
-        { this.renderCurrentGasPrice() }
-        <AreaGraph title="Transactions Activity" data={this.props.activity} />
-        <Map peers={this.props.peers} />
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    ...state
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    startUpdates: bindActionCreators(startUpdates, dispatch)
-  }
-}
-
-export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Dashboard)
+export default () => (
+  <div>
+    <ul>
+      <li><Link href="/dashboard"><a>Dashboard</a></Link></li>
+      <li><Link href="/hackerwatch"><a>Hacker Watcher</a></Link></li>
+    </ul>
+  </div>
+)
